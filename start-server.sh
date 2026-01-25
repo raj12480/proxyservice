6f126bac8401aac1dda90be38a28aa8436cf855f
raj12480 <158236009+raj12480@users.noreply.github.com>
2026-01-26 01:02:54 +0530

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-export FLASK_KEYCLOAK_AUTH_URL="http://13.51.237.239:8080/realms/service_desk"
+# ==============================
+# Environment Variables
+# ==============================
+
+# Keycloak authentication URL for the Flask app
+# NOTE: 
+#   - If you create a realm with a different name, replace 'service_desk' with your realm name.
+#   - Update <keyclock-host-ip> with the actual IP or hostname of your Keycloak server.
+#   - Change the port (8080) if your Keycloak server is running on a different port.
+# Example URL format: http://127.0.0.1:8080/realms/my_custom_realm
+export FLASK_KEYCLOAK_AUTH_URL="http://<keyclock-host-ip>:8080/realms/service_desk"
+
+# Zoho OAuth2 Client ID
 export FLASK_ZOHO_CLIENT_ID="1000.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Zoho OAuth2 Client Secret
 export FLASK_ZOHO_CLIENT_SECRET="xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 
